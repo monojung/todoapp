7625895bfbbb0936baff318ed25479c12d7f6ee5
Rithipoom Ariyajon <goldcyber.fenz@gmail.com>
2021-06-13 15:57:51 +0700

    แก้ สุดท้ายแล้ว

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
-    public function posts(){
+    public function Activity(){
         return $this->hasMany(Activity::class);
       }
 
